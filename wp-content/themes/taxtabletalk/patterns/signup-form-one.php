84c87f83b59ef1c9ca63f69cd98e71bdf5b197fa
Stephen Osunwale <hostephen@hodessy.com>
2023-01-24 00:53:56 +0100

    Finalize Files and Design
    Top pages completed and patterns updates

--- wp-content/themes/taxtabletalk/patterns/signup-form-one.php
+++ wp-content/themes/taxtabletalk/patterns/signup-form-one.php
@@ -2,22 +2,24 @@
 /**
  * Title: Sign Up v1.
  * Slug: taxtabletalk/signup-form-one
- * Categories: column,cta
+ * Categories: column, buttons
  */
 ?>
-<!-- wp:group {"style":{"color":{"gradient":"linear-gradient(90deg,rgb(255,255,255) 51%,rgb(238,238,238) 51%)"}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-background" style="background:linear-gradient(90deg,rgb(255,255,255) 51%,rgb(238,238,238) 51%)"><!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|50","left":"var:preset|spacing|50"}}}} -->
-<div class="wp-block-columns are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"36.5%","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}}} -->
-<div class="wp-block-column is-vertically-aligned-center" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);flex-basis:36.5%"><!-- wp:heading {"style":{"typography":{"lineHeight":"1"}},"fontSize":"xx-large"} -->
+<!-- wp:group {"className":"signup-cta-group","layout":{"type":"default"}} -->
+<div id="cta" class="wp-block-group signup-cta-group"><!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|50","left":"var:preset|spacing|50"},"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"backgroundColor":"custom-base"} -->
+<div class="wp-block-columns are-vertically-aligned-center has-custom-base-background-color has-background" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:column {"verticalAlignment":"center","width":"40%","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","right":"var:preset|spacing|40","left":"var:preset|spacing|40"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40);flex-basis:40%"><!-- wp:heading {"style":{"typography":{"lineHeight":"1"}},"fontSize":"xx-large"} -->
 <h2 class="has-xx-large-font-size" style="line-height:1">Join us for the real conversation</h2>
-<!-- /wp:heading --></div>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Signup to our newsletter varius vel pharetra vel turpis. Sed sed risus pretium quam</p>
+<!-- /wp:paragraph --></div>
 <!-- /wp:column -->
 
-<!-- wp:column {"verticalAlignment":"center","width":"60%"} -->
-<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:60%"><!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:heading {"textAlign":"left","className":"is-style-outline-border"} -->
-<h2 class="has-text-align-left is-style-outline-border">Form</h2>
-<!-- /wp:heading --></div>
+<!-- wp:column {"verticalAlignment":"center","width":"60%","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50"}}},"backgroundColor":"custom-secondary","textColor":"custom-base","layout":{"type":"constrained"}} -->
+<div class="wp-block-column is-vertically-aligned-center has-custom-base-color has-custom-secondary-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50);flex-basis:60%"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:fluentfom/guten-block {"formId":"3"} /--></div>
 <!-- /wp:group --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>
